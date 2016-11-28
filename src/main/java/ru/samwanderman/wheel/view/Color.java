c6491f8a1f5b41e0f858dc90429a433aa23cdfdc
Potapov SA <sam-wanderman@yandex.ru>
2016-11-28 16:19:43 +0300

    [+] added new color const

--- src/main/java/ru/samwanderman/wheel/view/Color.java
+++ src/main/java/ru/samwanderman/wheel/view/Color.java
@@ -7,6 +7,7 @@ public final class Color {
 	public static final Color GRAY = new Color(127, 127, 127);
 	public static final Color GREEN = new Color(0, 255, 0);
 	public static final Color RED = new Color(255, 0, 0);
+	public static final Color YELLOW = new Color(255, 255, 0);
 	
 	private int r;
 	private int g;
